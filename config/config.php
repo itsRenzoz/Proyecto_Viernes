493d106a6b989d7ddbfb3c452bab97fce3329ba7
itsrenzoz <aarondavidsalas@hotmail.com>
2025-08-12 22:51:40 -0600

    Implementación de variables CSS y fuentes de Google Fonts

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 // Se pueden sobrescribir con variables de entorno en XAMPP si se desea
 
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-define('DB_PORT', getenv('DB_PORT') ?: '3306');
+define('DB_PORT', getenv('DB_PORT') ?: '3308');
 define('DB_NAME', getenv('DB_NAME') ?: 'vetagenda');
 define('DB_USER', getenv('DB_USER') ?: 'root');
 define('DB_PASS', getenv('DB_PASS') ?: '123456');
